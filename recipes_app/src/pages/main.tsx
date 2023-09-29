1ef8fe77b5c02841e40cd5a40a22ef8ead6cd8bc
Kirsten-Elise Hanssen Rensaa <kerensaa@stud.ntnu.no>
2023-09-29 15:59:32 +0200

    Revert "Create basic navbar"
    This reverts commit 04a1ea83a9423e783526a4f25cf3f7dc0bf157b6

--- recipes_app/src/pages/main.tsx
+++ recipes_app/src/pages/main.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../styling/index.css'
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from './LandingPage.tsx'
-import Navbar from '../components/Navbar.tsx';
+import '../styling/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-      </Routes>
-    </Router>
+    <LandingPage />
   </React.StrictMode>,
 )
