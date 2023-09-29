04a1ea83a9423e783526a4f25cf3f7dc0bf157b6
kerensaa <kerensaa@stud.ntnu.no>
2023-09-29 15:53:08 +0200

    Create basic navbar

---
+++ recipes_app/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styling/Navbar.css'
+
+function Navbar() {
+    return (
+      <div className='navbar-container'>
+        <nav className='nav'>
+        <div ><h2><a href="/" className='link'>Recipes</a></h2></div>
+        </nav>
+      </div>
+    );
+  }
+export default Navbar
--- recipes_app/src/pages/main.tsx
+++ recipes_app/src/pages/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LandingPage from './LandingPage.tsx'
 import '../styling/index.css'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import LandingPage from './LandingPage.tsx'
+import Navbar from '../components/Navbar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LandingPage />
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
