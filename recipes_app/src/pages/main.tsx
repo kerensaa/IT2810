a0798fc1e358350a4a3f6fb53eab335f9acf5d48
Ashild Tenold Fridtun <aashiltf@stud.ntnu.no>
2023-10-02 13:44:24 +0200

    #3 Fix typo in import statement

--- recipes_app/src/pages/main.tsx
+++ recipes_app/src/pages/main.tsx
@@ -4,7 +4,7 @@ import LandingPage from './LandingPage.tsx'
 import '../styling/index.css'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from '../components/Navbar.tsx';
-import Recipe from './recipe.tsx';
+import Recipe from './Recipe.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
