dadb94c791bd977664e540e8628b1e18718c8743
kerensaa <kerensaa@stud.ntnu.no>
2023-10-04 11:25:24 +0200

    Add heart button components

--- recipes_app/src/components/recipeElement.tsx
+++ recipes_app/src/components/recipeElement.tsx
@@ -1,4 +1,6 @@
 import { Rate } from 'antd';
+import Heart from './Heart';
+import "../styling/HeartButton.css"
 
 interface RecipeElementProps{
     imagePath: string,
@@ -8,15 +10,20 @@ interface RecipeElementProps{
 
 export default function RecipeElement(props: RecipeElementProps){
     const {imagePath, title, description} = props;
+
     
     return (
         <section className="recipe-element">
             <img className="recipe-image" src={imagePath} alt={title} height={300}/>
             <div className="recipe-title"> 
             <h3 >{title}</h3>
+            <div className='rate-and-favorite'>
             <Rate defaultValue={0}></Rate>
+            <div className='button-placement'><button className="h-container"><Heart></Heart></button></div>
+            </div>
             </div>
             <p className="recipe-info"> {description}</p>
+            
         </section>
     )
 }
