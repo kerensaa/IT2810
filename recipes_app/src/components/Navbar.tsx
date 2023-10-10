5603e584a769c8998b4518b1547068589eac71d6
pantea joo <panteaj@stud.ntnu.no>
2023-10-10 13:48:23 +0200

    formatted files using prettier

--- recipes_app/src/components/Navbar.tsx
+++ recipes_app/src/components/Navbar.tsx
@@ -1,19 +1,25 @@
-import '../styling/Navbar.css'
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "../styling/Navbar.css";
 
 function Navbar() {
-    return (
-      <div className='navbar-container'>
-        <nav className='nav'>
-        <div className='left-content'>
-            <h2><Link to="/" >Recipes4You</Link></h2>
+  return (
+    <div className="navbar-container">
+      <nav className="nav">
+        <div className="left-content">
+          <h2>
+            <Link to="/">Recipes4You</Link>
+          </h2>
         </div>
-        <div className='right-content'>
-          <text><Link to="/">My Ratings</Link></text>
-          <text><Link to="/">My Favorites</Link></text>
+        <div className="right-content">
+          <text>
+            <Link to="/">My Ratings</Link>
+          </text>
+          <text>
+            <Link to="/">My Favorites</Link>
+          </text>
         </div>
-        </nav>
-      </div>
-    );
-  }
-export default Navbar
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
