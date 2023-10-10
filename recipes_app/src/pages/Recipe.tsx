ba26ed17994997a407aa1170787b8649ee8b5dbf
pantea joo <panteaj@stud.ntnu.no>
2023-10-10 11:57:54 +0200

    #18 added styling on individual recipe pages

--- recipes_app/src/pages/Recipe.tsx
+++ recipes_app/src/pages/Recipe.tsx
@@ -1,24 +1,118 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Rating,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import Heart from "../components/Heart";
 import { mockUsers } from "../mockData/mockData";
 
 export default function Recipe() {
-  // get the current id from the link
   const { recipeId } = useParams();
-  // convert  id from string to number
-  const recipeIdNum: number = parseInt(recipeId!, 10);
-  // get the matching object of the recipe id
+  const recipeIdNum = parseInt(recipeId!, 10);
   const matchedRecipe = mockUsers.find((recipe) => recipe.id === recipeIdNum);
 
   const handleGoBack = () => {
     history.back();
   };
+
+  const formattedIngredients = matchedRecipe?.ingredients
+    ? matchedRecipe.ingredients
+        .split(",")
+        .map((ingredient) => ingredient.trim().replace(/"/g, ""))
+    : [];
+  const [rating, setRating] = useState<number | null>(null);
+
+  const handlePostComment = () => {
+    // Handle posting a comment here
+  };
+
   return (
     <>
-      <button onClick={handleGoBack}>Back</button>
-      <img src={matchedRecipe?.icon_path} alt={matchedRecipe?.icon_path}></img>
-      <h1>{matchedRecipe?.title}</h1>
-      <h2>{matchedRecipe?.description}</h2>
-      <h3>{matchedRecipe?.ingredients}</h3>
+      <button className="back-button" onClick={handleGoBack}>
+        Back
+      </button>
+      <img src={matchedRecipe?.icon_path} alt={matchedRecipe?.icon_path} />
+      <div className="card-container">
+        <div className="left-column">
+          <div className="ingredients-card">
+            <Card style={{ backgroundColor: "#F5EDF7" }}>
+              <CardContent>
+              <h3>Ingredients:</h3>
+              <div>
+                {formattedIngredients.map((ingredient, index) => (
+                  <div key={index}>{ingredient}</div>
+                ))}
+              </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="rating-card">
+            <Card style={{ backgroundColor: "#F5EDF7", marginTop: "20px" }}>
+              <CardContent>
+                <Typography variant="h6">
+                  What did you think about this recipe?
+                </Typography>
+                <Box mt={2}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <div>
+                      <Rating
+                        name="recipe-rating"
+                        value={rating}
+                        onChange={(_event, newValue) => {
+                          setRating(newValue);
+                        }}
+                      />
+                    </div>
+                    <button className="like-button">
+                      <Heart></Heart>
+                    </button>
+                  </div>
+                </Box>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <textarea
+                    placeholder="Add comment..."
+                    rows={4}
+                    style={{
+                      width: "100%",
+                      fontSize: "16px",
+                      border: "1px solid #ccc",
+                    }}
+                  />
+                  <Button
+                    variant="contained"
+                    style={{
+                      backgroundColor: "#C5C6EF",
+                      marginLeft: "10px",
+                    }}
+                    onClick={handlePostComment}
+                  >
+                    Post
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className="right-column">
+          <Card style={{ backgroundColor: "#F5EDF7" }}>
+            <CardContent className="recipe-card-content">
+              <h2>{matchedRecipe?.title}</h2>
+              <p>{matchedRecipe?.description}</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
