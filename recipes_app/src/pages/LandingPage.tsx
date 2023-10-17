6864562f161622e49b50f6f0f26560c95c0b8e5d
kerensaa <kerensaa@stud.ntnu.no>
2023-10-17 15:06:38 +0200

    #1 Switch Input to Autocomplete

--- recipes_app/src/pages/LandingPage.tsx
+++ recipes_app/src/pages/LandingPage.tsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import RecipeElement from "../components/recipeElement";
 import { mockUsers } from "../mockData/mockData";
 import "../styling/LandingPage.css";
 import "../styling/recipeElement.css";
-import { Input } from "antd";
+import { Autocomplete, TextField } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 
+
+
 function LandingPage() {
-  const { Search } = Input;
+  const [searchResults, setSearchResults] = useState("");
+
+  function SearchFunction(event, values) {
+    const recipeResults = mockUsers.filter((recipe) => recipe.title.toLowerCase().includes(values.toLowerCase()))
+  }
 
   return (
     <>
       <h1></h1>
-      <Search placeholder="Search" style={{ paddingTop: 40, maxWidth: 600 }} />
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={mockUsers}
+        sx={{ paddingTop: 10, maxWidth: 600}}
+        onChange={(event, newValue) => SearchFunction(event, newValue)}
+        renderInput={(params) => <TextField {...params} label="Search" />}
+      />
       <section className="recipe-grid">
         {mockUsers.map((recipe) => (
           <div className="recipe-element" key={recipe.id}>
