8602b2cede06648ca8964d21d8a43e24b363ba2d
pantea joo <panteaj@stud.ntnu.no>
2023-10-17 13:52:53 +0200

    #24 added comment section

--- recipes_app/src/pages/Recipe.tsx
+++ recipes_app/src/pages/Recipe.tsx
@@ -117,24 +117,20 @@ export default function Recipe() {
           </Card>
         </div>
           <Container>
-            <div className="comment-section">
-              <Accordion>
-              <AccordionSummary
-          
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
+              <Accordion className="comment-section">
+                  <AccordionSummary
+                  
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography>See comments</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    
+                  </Typography>
+                </AccordionDetails>
               </Accordion>
-
-            </div>
           </Container>
       </div>
     </>
