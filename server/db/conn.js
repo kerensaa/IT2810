340c4a07fd7af1c448978489e016ec9b898304e1
alfhusem <alfbhusem@gmail.com>
2023-10-18 20:10:50 +0200

    set up and connect to MongoDB

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,24 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const client = new MongoClient(Db);
  
 var _db;
  
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
+    connectToServer: function (callback) {
+        client.connect()
+        .then(db => {
+          _db = db.db("recipe_db");
+          console.log("Successfully connected to MongoDB.");
+          callback(null);
+        })
+        .catch(err => {
+          console.error("Error connecting to MongoDB:", err);
+          callback(err);
+        });
+        },
  
-  getDb: function () {
-    return _db;
-  },
+    getDb: function () {
+        return _db;
+    },
 };
