a7b66a5358065b92f0e8af945c02c027c8258d26
Ashild Tenold Fridtun <aashiltf@stud.ntnu.no>
2023-10-19 14:31:25 +0200

    #31 Make component for LandingPage for reuse

---
+++ recipes_app/src/components/LandingPageTemplate.tsx
@@ -0,0 +1,49 @@
+import RecipeElement from '../components/recipeElement';
+import { Recipe } from '../mockData/mockData';
+import '../styling/LandingPage.css';
+import '../styling/recipeElement.css';
+import { Input } from 'antd';
+import Pagination from '@mui/material/Pagination';
+import { usePagination } from '../utils/paginationUtils';
+
+interface LandingPageTemplateProps {
+  dataSource: Recipe[];
+}
+
+function LandingPageTemplate(props: LandingPageTemplateProps) {
+  const { Search } = Input;
+
+  // pagination state, variables and functions
+  const elementsPerPage: number = 3;
+  const { currentPage, elementsDisplayed, handlePageChange } = usePagination(1, elementsPerPage, props.dataSource);
+
+  return (
+    <>
+      <h1></h1>
+      <Search placeholder="Search" style={{ paddingTop: 40, maxWidth: 600 }} />
+      <section className="recipe-grid">
+        {elementsDisplayed.map((recipe) => (
+          <div className="recipe-element" key={recipe.id}>
+            <RecipeElement
+              recipeID={recipe.id}
+              imagePath={recipe.icon_path}
+              title={recipe.title}
+              description={recipe.description}
+            />
+          </div>
+        ))}
+      </section>
+      <div className="pagination-container">
+        <Pagination
+          count={Math.ceil(props.dataSource.length / elementsPerPage)}
+          color="secondary"
+          shape="rounded"
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </div>
+    </>
+  );
+}
+
+export default LandingPageTemplate;
--- recipes_app/src/pages/LandingPage.tsx
+++ recipes_app/src/pages/LandingPage.tsx
@@ -1,43 +1,10 @@
-import RecipeElement from '../components/recipeElement';
 import { mockUsers } from '../mockData/mockData';
-import '../styling/LandingPage.css';
-import '../styling/recipeElement.css';
-import { Input } from 'antd';
-import Pagination from '@mui/material/Pagination';
-import { usePagination } from '../utils/paginationUtils';
+import LandingPageTemplate from '../components/LandingPageTemplate';
 
 function LandingPage() {
-  const { Search } = Input;
-
-  // pagination state, variables and functions
-  const elementsPerPage: number = 3;
-  const { currentPage, elementsDisplayed, handlePageChange } = usePagination(1, elementsPerPage, mockUsers);
-
   return (
     <>
-      <h1></h1>
-      <Search placeholder="Search" style={{ paddingTop: 40, maxWidth: 600 }} />
-      <section className="recipe-grid">
-        {elementsDisplayed.map((recipe) => (
-          <div className="recipe-element" key={recipe.id}>
-            <RecipeElement
-              recipeID={recipe.id}
-              imagePath={recipe.icon_path}
-              title={recipe.title}
-              description={recipe.description}
-            />
-          </div>
-        ))}
-      </section>
-      <div className="pagination-container">
-        <Pagination
-          count={Math.ceil(mockUsers.length / elementsPerPage)}
-          color="secondary"
-          shape="rounded"
-          page={currentPage}
-          onChange={handlePageChange}
-        />
-      </div>
+      <LandingPageTemplate dataSource={mockUsers} />
     </>
   );
 }
