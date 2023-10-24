ba0e06e49142826a947301e0406e6ee9dbee0ebd
pantea joo <panteaj@stud.ntnu.no>
2023-10-24 16:22:41 +0200

    #25 added delete and spacing

--- recipes_app/src/pages/Recipe.tsx
+++ recipes_app/src/pages/Recipe.tsx
@@ -1,3 +1,4 @@
+import DeleteIcon from '@mui/icons-material/Delete';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {
   Accordion,
@@ -8,14 +9,17 @@ import {
   Card,
   CardContent,
   Container,
+  IconButton,
+  Paper,
   Typography,
 } from '@mui/material';
-import { SetStateAction, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CommentsDB from '../components/CommentsDB';
 import Favorite from '../components/Favorites';
 import Ratings from '../components/Ratings';
 import { mockUsers } from '../mockData/mockData';
+const [isLoading, setIsLoading] = useState(true);
 
 export default function Recipe() {
   const { recipeId } = useParams();
@@ -30,17 +34,35 @@ export default function Recipe() {
     ? matchedRecipe.ingredients.split(',').map((ingredient) => ingredient.trim().replace(/"/g, ''))
     : [];
   const [comment, setComment] = useState('');
-  const { comments, addComment } = CommentsDB();
+  const { comments, addComment, deleteComment } = CommentsDB();
 
-  const filteredComments = comments.filter((comment) => comment.recipeId === recipeIdNum);
+  const filteredComments = comments.filter((c) => c.recipeId === recipeIdNum);
 
   const handleCommentChange = (event: { target: { value: SetStateAction<string> } }) => {
     setComment(event.target.value);
   };
+
   const postComment = () => {
     if (comment.trim() !== '') {
       addComment(recipeIdNum, comment);
       setComment('');
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      setIsLoading(false);
+    }
+
+    if (!comments || comments.length === 0) {
+      console.error('Comments not loaded yet.');
+    }
+  }, [comments, isLoading]);
+  const handleDeleteComment = (commentId: number | undefined) => {
+    if (commentId !== undefined) {
+      deleteComment(commentId);
+    } else {
+      console.error('Invalid commentId:', commentId);
     }
   };
 
@@ -107,17 +129,36 @@ export default function Recipe() {
               </CardContent>
             </Card>
           </div>
-          <Container>
-            <Accordion className="comment-section" style={{ backgroundColor: '#BB99CD', width: 'auto' }}>
+          <Container style={{ display: 'flex', justifyContent: 'center' }}>
+            <Accordion className="comment-section" style={{ backgroundColor: '#BB99CD', width: '60%' }}>
               <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
                 <Typography>See comments</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <div>
-                  {filteredComments.map((comment, index) => (
-                    <div key={comment.id}>{comment.text}</div>
-                  ))}
-                </div>
+                {filteredComments.map((comment) => (
+                  <div key={comment.id} style={{ position: 'relative' }}>
+                    <Paper
+                      sx={{
+                        m: 2,
+                        whiteSpace: 'pre-line',
+                        width: '95%',
+                        overflow: 'visible',
+                      }}
+                    >
+                      <div style={{ display: 'flex', position: 'relative' }}>
+                        <IconButton
+                          size="small"
+                          color="secondary"
+                          style={{ position: 'absolute', top: '0', right: '0' }}
+                          onClick={() => handleDeleteComment(comment.id)}
+                        >
+                          <DeleteIcon fontSize="small" />
+                        </IconButton>
+                      </div>
+                      <div style={{ wordWrap: 'break-word', padding: '30px 0 0 0' }}>{comment.text}</div>
+                    </Paper>
+                  </div>
+                ))}
               </AccordionDetails>
             </Accordion>
           </Container>
