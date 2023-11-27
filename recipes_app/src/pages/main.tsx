54ab671e31ef64a7c4adb1dce84895e4e9a160ac
Ashild Tenold Fridtun <aashiltf@stud.ntnu.no>
2023-11-27 16:07:30 +0100

    Add right path

--- recipes_app/src/pages/main.tsx
+++ recipes_app/src/pages/main.tsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Router>
         <Navbar></Navbar>
         <Routes>
-          <Route path="/project2/project2" element={<LandingPage />} />
+          <Route path="/project2/" element={<LandingPage />} />
           <Route path="/project2/project2/:recipeId" element={<Recipe />} />
           <Route path="/project2/project2/MyFavorites" element={<MyFavorites />} />
           <Route path="/project2/project2/MyRatings" element={<MyRatings />} />
